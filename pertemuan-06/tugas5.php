38c95140d2850a6ee4272a358ac4d3d276636278
Muhammad Zaky Syanov Ziyad <2511500057@mahasiswa.atmaluhur.ac.id>
2025-11-05 22:22:22 +0700

    Misahin file tugas biar mudah di liat

--- pertemuan-06/tugas5.php
+++ pertemuan-06/tugas5.php
@@ -6,27 +6,10 @@
   <meta name="viewport" content="width=, initial-scale=1.0">
   <title>Judul Halaman</title>
   <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
 </head>
 
 <body>
-  <header>
-    <h1>Ini Header</h1>
-    <button class="menu-toggle" id="menuToggle" aria-label="Toggle Navigaton">
-        &#9776;
-    </button>
-    <nav>
-      <ul>
-        <li><a href="#home">beranda</a></li>
-        <li><a href="#about">tentang</a></li>
-        <li><a href="#contact">kontak</a></li>
-      </ul>
-    </nav>
-  </header>
-  <main>
-    <section id="home">
-      <h2>Selamat Datang</h2>
-      <p>ini contoh paragraf html</p>
-    </section>
     <section id="about">
       <?php
         $nim = "2511500086";
@@ -130,31 +113,5 @@
             ?>
         </p>
     </section>
-    <section id="contact">
-      <h2>kontak kami</h2>
-      <form action="" method="GET">
-        <label for="txtNama"><span>Nama:</span>
-        <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama">
-        </label>
-
-        <label for="txtEmail"><span>Email:</span>
-        <input type="email" id="txtEmail" name="txtEmail" placeholder="Masukkan email">
-        </label>
-
-        <label for="txtPesan"><span>Pesan:</span>
-        <textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis pesan anda..."></textarea>
-        <small id="charCount">0/200 karakter</small>
-        </label>
-
-        <button type="submit">Kirim</button>
-        <button type="reset">Batal</button>
-      </form>
-    </section>
-  </main>
-  <footer>
-    <p>&copy; &#9786; 2025 Muhammad Zaky Syanov Ziyad</p>
-  </footer>
-    <script src="script.js"></script>
 </body>
-
 </html>
